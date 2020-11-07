affe04abc1d9fd863c5282683c61b82d7f88a119
mkleung <mkleung@dummy.com>
2020-11-07 19:40:08 +0800

    Initial Commit

---
+++ SpeechRecognition/SpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionApp.swift
+//  SpeechRecognition
+//
+//  Created by Man Kin Leung on 7/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
